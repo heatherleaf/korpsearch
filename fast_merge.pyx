f4f3682de79940f049a682221fad44474af55c05
Peter Ljunglöf <peter.ljunglof@heatherleaf.se>
2024-05-31 22:22:16 +0200

    fast_merge: some optimisations

--- fast_merge.pyx
+++ fast_merge.pyx
@@ -1,19 +1,27 @@
 # cython: language_level=3
 
-from disk import LowlevelIntArray
 import sys
-
 from libc.stdlib cimport malloc, free
+
+from disk import LowlevelIntArray
+
 
 ctypedef const unsigned char[::1] buffer
 
 
-cdef buffer to_buffer(array, start, length):
+# TODO:
+#  1. Cython has memoryviews too, so it should be possible to use them directly 
+#     instead of working with byte buffers.
+#  2. We want to be able to update a set in-place of creating a new every time,
+#     just as is already done in Python (see IndexSet._merge_internal)
+
+
+cdef buffer to_buffer(array, size_t start, size_t length, size_t size):
     """Convert an array to an array of bytes."""
-    assert array._byteorder == sys.byteorder
-    start *= array._elemsize
-    length *= array._elemsize
-    return array._mmap[start : start+length]
+    start *= size
+    length *= size
+    return array._bytearray[start : start+length]
+
 
 def merge(arr1, start1, length1, offset1, arr2, start2, length2, offset2, take_first, take_second, take_common):
     """Merge two sorted arrays A and B.
@@ -31,46 +39,56 @@ def merge(arr1, start1, length1, offset1, arr2, start2, length2, offset2, take_f
     """
 
     assert arr1._elemsize == arr2._elemsize
-    cdef int size = arr1._elemsize
+    cdef int elemsize = arr1._elemsize
 
-    cdef buffer buf1 = to_buffer(arr1, start1, length1)
-    cdef buffer buf2 = to_buffer(arr2, start2, length2)
-
-    cdef const unsigned char* in1 = &buf1[0]
-    cdef const unsigned char* in2 = &buf2[0]
-    cdef size_t len1 = buf1.nbytes
-    cdef size_t len2 = buf2.nbytes
-
+    cdef buffer buf1 = to_buffer(arr1, start1, length1, elemsize)
+    cdef buffer buf2 = to_buffer(arr2, start2, length2, elemsize)
     # TODO: 
-    # if not (take_first or take_second): we can use malloc(min(len1, len2))
-    # if not take_second: we can use malloc(len1)
-    # if not take_first: we can use malloc(len2)
-    out = <char*>malloc(len1 + len2)
+    # if not (take_first or take_second): we can use outsize = min(len1, len2))
+    # if not take_second: we can use outsize = len1
+    # if not take_first: we can use outsize = len2
+    cdef size_t outsize = buf1.nbytes + buf2.nbytes
+    cdef unsigned char* out = <unsigned char*> malloc(outsize)
+
+    k = fast_merge(out, elemsize, 
+                   &buf1[0], start1, buf1.nbytes, offset1,
+                   &buf2[0], start2, buf2.nbytes, offset2,
+                   take_first, take_second, take_common)
+
+    result = LowlevelIntArray(out[:k], elemsize)
+    free(out)
+    return result
+
+
+cdef size_t fast_merge(unsigned char* out, size_t elemsize, 
+                       const unsigned char* in1, size_t start1, size_t len1, size_t offset1, 
+                       const unsigned char* in2, size_t start2, size_t len2, size_t offset2, 
+                       size_t take_first, size_t take_second, size_t take_common):
 
     cdef size_t i = 0
     cdef size_t j = 0
     cdef size_t k = 0
 
     while i < len1 and j < len2:
-        x = read_bytes(in1+i, size) - offset1
-        y = read_bytes(in2+j, size) - offset2
+        x = read_bytes(in1+i, elemsize) - offset1
+        y = read_bytes(in2+j, elemsize) - offset2
 
         if x < y: 
-            i += size
+            i += elemsize
             if take_first:
-                write_bytes(out+k, x, size)
-                k += size
+                write_bytes(out+k, x, elemsize)
+                k += elemsize
         elif x > y: 
-            j += size
+            j += elemsize
             if take_second:
-                write_bytes(out+k, y, size)
-                k += size
+                write_bytes(out+k, y, elemsize)
+                k += elemsize
         else:
-            i += size
-            j += size
+            i += elemsize
+            j += elemsize
             if take_common:
-                write_bytes(out+k, x, size)
-                k += size
+                write_bytes(out+k, x, elemsize)
+                k += elemsize
 
     if take_first:
         memcpy(out+k, in1+i, len1-i)
@@ -79,22 +97,20 @@ def merge(arr1, start1, length1, offset1, arr2, start2, length2, offset2, take_f
         memcpy(out+k, in2+j, len2-j)
         k += len2-j
 
-    result = LowlevelIntArray(bytemap=out[:k], elemsize=size, byteorder=sys.byteorder)
-    free(out)
-    return result
+    return k
 
 
 cdef extern from "string.h":
     void *memcpy(void *dest, const void *src, size_t len)
 
 
-cdef inline size_t read_bytes(const void *ptr, int size):
+cdef inline size_t read_bytes(const void *ptr, size_t size):
     """Read an integer of the given number of bytes from a pointer."""
     cdef size_t result = 0
     memcpy(&result, ptr, size)
     return result
 
 
-cdef inline void write_bytes(void *ptr, size_t value, int size):
+cdef inline void write_bytes(void *ptr, size_t value, size_t size):
     """Write an integer of the given number of bytes to a pointer."""
     memcpy(ptr, &value, size)
